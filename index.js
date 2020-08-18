fb245cb2605ddb7f76e654840f8b6b64fa2ed6b4
Nikolaos Tsoukas <nick.tsoukas101@gmail.com>
2020-08-18 10:37:32 -0500

    Adding number picker component

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-
-function getMood(){
-    const moods = ['Angry', 'Sad', 'Silly', 'Happy'];
-    return moods[Math.floor(Math.random() * moods.length)];
+function getNumber(){
+    return Math.floor(Math.random() * 100);
 }
 
-class JSXDemo extends React.Component {
-    render(){
+class NumPicker extends React.Component {
+    render() {
         return (
-            <div>
-                <h1> My Current Mood is : {getMood()} </h1>
-                <img style={{width: '300px'}} src="https://images.unsplash.com/photo-1597694491427-25b7e5c0946e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80" alt=""/>
-            </div>
+            <h1>Your number is .... { getNumber() }</h1>
         )
     }
 }
 
-ReactDOM.render(<JSXDemo/>, document.getElementById('root'));
+ReactDOM.render(<NumPicker/>, document.getElementById('root'));
