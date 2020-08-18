72fd97c62615e605559d3b61dea58da0de6cc48c
Nikolaos Tsoukas <nick.tsoukas101@gmail.com>
2020-08-18 10:27:01 -0500

    ...

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+
+function getMood(){
+    const moods = ['Angry', 'Sad', 'Silly', 'Happy'];
+    return moods[Math.floor(Math.random() * moods.length)];
+}
+
 class JSXDemo extends React.Component {
     render(){
         return (
             <div>
-                <h1>This is the demo component</h1>
+                <h1> My Current Mood is : {getMood()} </h1>
                 <img style={{width: '300px'}} src="https://images.unsplash.com/photo-1597694491427-25b7e5c0946e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80" alt=""/>
             </div>
         )
